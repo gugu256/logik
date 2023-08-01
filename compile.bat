19dbee799029c9af1c1dd146d5b89023c780950c
gugu256 <gugu256@mail.com>
2023-08-01 13:13:34 +0200

    Added an error when there are no input files

--- compile.bat
+++
@@ -1,5 +0,0 @@
-cmd
-g++ src/main.cpp
-cp a.exe build/logik.exe
-cp a.exe C:\Users\Gustave\Documents\code\bin\logik.exe
-del a.exe
---
+++ compile.sh
@@ -0,0 +1,4 @@
+g++ src/main.cpp
+cp a.exe build/logik.exe
+cp a.exe "C:\Users\Gustave\Documents\code\bin\logik.exe"
+rm a.exe
